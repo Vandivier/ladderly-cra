f34d78a77d758f6eb980c73107f4d37dc754f2e0
Vandivier <john@afterecon.com>
2020-10-11 17:52:34 -0400

    feat: home icons

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ one of two ladderly.io projects; this one using redux + create react app
 ## Some Style Rules
 
 1. All file names are downcase.
+2. Cards and Tiles are foundational, with distinct use cases: https://blog.prototypr.io/common-material-design-bad-practices-to-avoid-b7995f251329
--- ladderly-cra/src/app.tsx
+++ ladderly-cra/src/app.tsx
@@ -2,25 +2,110 @@ import "./app.css";
 
 import React from "react";
 
+// import logo from "./logo.svg";
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  makeStyles,
+  Paper,
+} from "@material-ui/core";
+import MonetizationOnIcon from "@material-ui/icons/MonetizationOn";
+import PersonAddIcon from "@material-ui/icons/PersonAdd";
+import SchoolIcon from "@material-ui/icons/School";
+
 import { Counter } from "./features/counter/counter";
 import { LadderlyTopNav } from "./features/ladderly-top-nav/ladderly-top-nav";
-import logo from "./logo.svg";
+import { SupportIcon } from "./features/support-icon/support-icon";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+  bigIcon: {
+    fontSize: "5rem",
+    height: "5rem",
+    width: "5rem",
+  },
+  featureCards: {
+    margin: "0px 8px",
+  },
+});
 
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
       <LadderlyTopNav />
 
       <header className="App-header">
-        Ladderly.io
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
+        <h1>Ladderly.io</h1>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
       </header>
 
+      <Container maxWidth="md" style={{ margin: "24px auto" }}>
+        <Grid container spacing={4}>
+          <Grid item className={classes.featureCards} component={Card} xs>
+            <CardActionArea>
+              <PersonAddIcon className={classes.bigIcon} />
+              <CardContent>Get Referrals for Work or School</CardContent>
+            </CardActionArea>
+
+            <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Grid>
+
+          <Grid item className={classes.featureCards} component={Card} xs>
+            <CardActionArea>
+              <SchoolIcon className={classes.bigIcon} />
+              <CardContent>Learn Skills and Network</CardContent>
+            </CardActionArea>
+
+            <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Grid>
+
+          <Grid item className={classes.featureCards} component={Card} xs>
+            <CardActionArea>
+              {/* {console.log(classes.bigIcon)} */}
+              <SupportIcon className={classes.bigIcon} />
+              <CardContent>Help Others and Earn Side Income</CardContent>
+            </CardActionArea>
+
+            <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Grid>
+
+          <Grid item className={classes.featureCards} component={Card} xs>
+            <CardActionArea>
+              <MonetizationOnIcon className={classes.bigIcon} />
+              <CardContent>Strategic Wealth Management</CardContent>
+            </CardActionArea>
+
+            <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Grid>
+        </Grid>
+      </Container>
+
       <section>
-        <p>
-          Edit <code>src/app.tsx</code> and save to reload.
-        </p>
+        <Counter />
+      </section>
+
+      <section>
         <span>
           <span>Learn </span>
           <a
