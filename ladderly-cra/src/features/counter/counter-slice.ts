9e182638fd6adeee1676b8b61fb02f9826997aa9
Vandivier <john@afterecon.com>
2020-10-11 16:38:18 -0400

    feat: minor ts improvements

--- ladderly-cra/src/features/counter/counter-slice.ts
+++ ladderly-cra/src/features/counter/counter-slice.ts
@@ -1,5 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+type CounterSliceLocalState = {
+  counter: { value: number };
+};
+
 export const counterSlice = createSlice({
   name: "counter",
   initialState: {
@@ -39,6 +43,7 @@ export const incrementAsync = (amount: number) => (
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state) => state.counter.value)`
-export const selectCount = (state: any) => state.counter.value;
+export const selectCount = (state: CounterSliceLocalState) =>
+  state.counter.value;
 
 export default counterSlice.reducer;
